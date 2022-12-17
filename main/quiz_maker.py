43e805932beea43181f9c5174c2a716d1f4d2f82
firefly <siddiqui.m.312@gmail.com>
2022-12-17 19:05:36 +0000

    Create quiz_maker.py
    +added steps to make quiz

---
+++ main/quiz_maker.py
@@ -0,0 +1,7 @@
+import os
+
+#Step 0. User sets quiz parameters.
+    #Step 0.1 Select number of questions.
+    #Step 0.2 Select types of questions.
+#Step 1. get database of questions.
+#Step 2. create quiz based on User parameters.
