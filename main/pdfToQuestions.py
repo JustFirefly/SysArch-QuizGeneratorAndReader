4aed99fc891879971dc4c641f1fe05287ad81c97
firefly <siddiqui.m.312@gmail.com>
2022-12-17 20:29:13 +0000

    Update pdfToQuestions.py
    +created readPDF function
    +added TODO

--- main/pdfToQuestions.py
+++ main/pdfToQuestions.py
@@ -1,12 +1,28 @@
 """
 Contributors: JustFirefly,
 
-Desc: main.py is meant to take pdf files and turn them into text files that has one test question
+Desc: pdfToQuestions.py is meant to take pdf files and turn them into text files that has one test question
 """
 
 import os
 import fitz
 import PyPDF2
+
+#function reads through a pdf file and returns its contents - takes pdfFile as a parameter.
+#TODO find out how the hell to format the text into something readable. >> text is inconsistent in seperating the answers
+#                                                                       >> text has nonsense scattered between the questions and answers
+#                                                                       + need to scan through the pdf -> filter out answers, seem to always be seperated by periods ('.')
+#                                                                       + can filter Questions knowing there is exactly one question between to "Question" words in the PDF.
+def readPDF(pdfFile):        
+    pdf = open(pdfFile, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdf)
+    complete = ""
+    for p in range(pdfReader.numPages):
+        page = pdfReader.getPage(p)
+        #print(f'{("Page "+str(p)):-^100}')
+        #print(page.extractText())
+        complete+=page.extractText()
+    return complete
 
 #https://stackoverflow.com/questions/2693820/extract-images-from-pdf-without-resampling-in-python -- getting an image from a pdf (fitz)
 #https://www.youtube.com/watch?v=N6Su4Hk8_-g -- reading pdf files (PyPDF2)
@@ -16,17 +32,12 @@ import PyPDF2
 path = "..//PDFs_HERE//firefly_0826"
 os.chdir(path)
 list_of_files = os.listdir()
-print(list_of_files)
 
+#print(list_of_files)
 
 #Step 2. read through pdf file.
-def readPDF(pdfFile):        
-    pdf = open(pdfFile, 'rb')
-    pdfReader = PyPDF2.PdfFileReader(pdf)
-    for p in range(pdfReader.numPages):
-        page = pdfReader.getPage(p)
-        print(page.extractText())
 
+print(readPDF(list_of_files[0])) # prints the contents of list_of_files[0]
 
 #Step 3. filter what the questions and answers are.
 
