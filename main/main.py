dff59ca70cd2bb6f8c43f1867b864298e9809284
firefly <siddiqui.m.312@gmail.com>
2022-12-17 18:09:14 +0000

    updated main.py
    imported libraries need to get pdf files (reading and ripping images)

---
+++ main/main.py
@@ -0,0 +1,8 @@
+import os
+import fitz
+import PyPDF2
+
+#https://stackoverflow.com/questions/2693820/extract-images-from-pdf-without-resampling-in-python -- getting an image from a pdf (fitz)
+#https://www.youtube.com/watch?v=N6Su4Hk8_-g -- reading pdf files (PyPDF2)
+
+
