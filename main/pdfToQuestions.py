72333dc1ccf6b545078f978b7ce2f3494db39af9
firefly <siddiqui.m.312@gmail.com>
2022-12-17 20:03:30 +0000

    barebones of pdfToQuestions
    decompressed my pdf files
    having issues with formatting of txt files

--- main/pdfToQuestions.py
+++ main/pdfToQuestions.py
@@ -11,9 +11,27 @@ import PyPDF2
 #https://stackoverflow.com/questions/2693820/extract-images-from-pdf-without-resampling-in-python -- getting an image from a pdf (fitz)
 #https://www.youtube.com/watch?v=N6Su4Hk8_-g -- reading pdf files (PyPDF2)
 
-#Step 1. get pdf file.
+#Step 1. get pdf files.
+
+path = "..//PDFs_HERE//firefly_0826"
+os.chdir(path)
+list_of_files = os.listdir()
+print(list_of_files)
+
+
 #Step 2. read through pdf file.
+def readPDF(pdfFile):        
+    pdf = open(pdfFile, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdf)
+    for p in range(pdfReader.numPages):
+        page = pdfReader.getPage(p)
+        print(page.extractText())
+
+
 #Step 3. filter what the questions and answers are.
+
 #Step 4. format txt file into readable format for python.
+
 #Step 5. add contents to the txt file and add to Database folder.
+
 #Step 6. Profit.
