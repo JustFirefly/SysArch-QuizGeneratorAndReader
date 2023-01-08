6e0bf2ad2dc0d2b54a0e7503301c318463a9696d
JustFirefly <siddiqui.m.312@gmail.com>
2023-01-08 11:04:54 +0000

    can actually find questions now

--- main/pdfToQuestions.py
+++ main/pdfToQuestions.py
@@ -38,16 +38,20 @@ list_of_files.sort()
 #print(list_of_files)
 
 #Step 2. read through pdf file.
-pdf_content = extract_text(list_of_files[0])
-new = pdf_content.replace("\n\uf00c Correct.", "\n").replace("\n\uf00c Correct answer.", "\n").replace("\nCorrect\n", "\n").replace("\n", "").replace("\uf00c", "").replace("\uf00d", "")
-print(new)# prints the contents of list_of_files[0]
+pdf_content = extract_text(list_of_files[6])
+new = pdf_content.replace("\n\uf00c Correct.", "\n").replace("\n\uf00c Correct answer.", "\n").replace("\nCorrect\n", "\n").replace("\n", "").replace("\uf00c", "").replace("\uf00d", "").replace("Incorrect.", "").replace("Partially correct", "").replace("Not correct.","")
+#print(new)# prints the contents of list_of_files[0]
 
-q_pattern = re.compile(r'[abcde]\.\s[a-zA-Z0-9\'\,\/\s]*\s{1,3}')
-s_pattern = re.compile(r'1.00[a-zA-Z0-9\'\,\/\s\.\?]+\S')
-a_pattern = re.compile(r'[\s\S] This is wrong.')
+#q_pattern = re.compile(r'\S[abcdeg]\.\s[a-zA-Z0-9\'\,\/\s]*\s{1,3}')
+s_pattern = re.compile(r'uestion [0-9/s].+?.+?Q') #this one gets questions. all of it.
+#x_pattern = re.compile(r'Question\s[0-9\S]{2}[a-zA-Z0-9\'\,\/\s\.\?\:]+[one\:\?]{4}a\.\s')
+#a_pattern = re.compile(r'[\s\S] This is wrong.')
 result_q = s_pattern.findall(new)
-print(result_q)
+for entry in result_q:
+    print(f'{"":-^100}')
+    print(entry)
 print(len(result_q))
+
 #Step 3. filter what the questions and answers are.
 
 #Step 4. format txt file into readable format for python.
